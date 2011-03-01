2c4b4aea4e8d1526937252605c18f01e5995702c
Jeff Yu <jeff.yuchang@gmail.com>
2011-03-01 22:50:08 +1100

    RIFTSAW-344, add source tab for the ODE events

--- gui/war/src/main/java/org/jboss/bpm/console/client/process/InstanceDetailView.java
+++ gui/war/src/main/java/org/jboss/bpm/console/client/process/InstanceDetailView.java
@@ -21,20 +21,22 @@
  */
 package org.jboss.bpm.console.client.process;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.DeferredCommand;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Label;
-import com.mvc4g.client.Controller;
-import com.mvc4g.client.Event;
-import com.mvc4g.client.ViewInterface;
+import java.util.List;
+import java.util.StringTokenizer;
+
 import org.gwt.mosaic.ui.client.CaptionLayoutPanel;
+import org.gwt.mosaic.ui.client.DecoratedTabLayoutPanel;
+import org.gwt.mosaic.ui.client.ListBox;
+import org.gwt.mosaic.ui.client.ListBox.CellRenderer;
 import org.gwt.mosaic.ui.client.MessageBox;
+import org.gwt.mosaic.ui.client.ScrollLayoutPanel;
+import org.gwt.mosaic.ui.client.TabLayoutPanel;
+import org.gwt.mosaic.ui.client.TabLayoutPanel.TabBarPosition;
 import org.gwt.mosaic.ui.client.layout.BoxLayout;
 import org.gwt.mosaic.ui.client.layout.BoxLayoutData;
+import org.gwt.mosaic.ui.client.layout.LayoutPanel;
 import org.gwt.mosaic.ui.client.layout.MosaicPanel;
+import org.gwt.mosaic.ui.client.list.DefaultListModel;
 import org.jboss.bpm.console.client.ApplicationContext;
 import org.jboss.bpm.console.client.ServerPlugins;
 import org.jboss.bpm.console.client.common.PropertyGrid;
@@ -42,7 +44,21 @@ import org.jboss.bpm.console.client.common.WidgetWindowPanel;
 import org.jboss.bpm.console.client.model.ProcessDefinitionRef;
 import org.jboss.bpm.console.client.model.ProcessInstanceRef;
 import org.jboss.bpm.console.client.util.SimpleDateFormat;
+import org.jboss.bpm.monitor.gui.client.HistoryRecords;
+import org.jboss.errai.bus.client.api.RemoteCallback;
+import org.jboss.errai.bus.client.api.base.MessageBuilder;
 import org.jboss.errai.workspaces.client.framework.Registry;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.mvc4g.client.Controller;
+import com.mvc4g.client.Event;
+import com.mvc4g.client.ViewInterface;
 
 /**
  * @author Heiko.Braun <heiko.braun@jboss.com>
@@ -77,6 +93,8 @@ public class InstanceDetailView extends CaptionLayoutPanel implements ViewInterf
     private ProcessDefinitionRef currentDefintion;
 
     private boolean isRiftsawInstance;
+    
+    private  ListBox<String> processEvents;
 
     public InstanceDetailView()
     {
@@ -160,20 +178,75 @@ public class InstanceDetailView extends CaptionLayoutPanel implements ViewInterf
     private void createDiagramWindow(ProcessInstanceRef inst)
     {
         
-        MosaicPanel layout = new MosaicPanel(new BoxLayout(BoxLayout.Orientation.VERTICAL));
+    	org.gwt.mosaic.ui.client.layout.LayoutPanel layout = new ScrollLayoutPanel();
         layout.setStyleName("bpm-window-layout");
         layout.setPadding(5);
 
         Label header = new Label("Instance: "+inst.getId());
         header.setStyleName("bpm-label-header");
         layout.add(header, new BoxLayoutData(BoxLayoutData.FillStyle.HORIZONTAL));
+        
+        final TabLayoutPanel tabPanel = new DecoratedTabLayoutPanel(TabBarPosition.TOP);
+        tabPanel.setPadding(5);
+        
+        LayoutPanel diaViewLayout = new LayoutPanel();
+        diaViewLayout.add(diagramView);
+        
+        tabPanel.add(diaViewLayout, "View");
+  
+        processEvents = new ListBox<String>(new String[]{"Process Events"});
+        processEvents.setCellRenderer(new CellRenderer<String>(){
 
-        layout.add(diagramView, new BoxLayoutData(BoxLayoutData.FillStyle.BOTH));
+			@Override
+			public void renderCell(ListBox<String> listBox, int row,
+					int column, String item) {
+				switch(column) {
+				case 0:
+					listBox.setWidget(row, column, new HTML(item));
+					break;
+				default:
+					throw new RuntimeException("Should not happen!");
+				}				
+			}
+        });
+        
+        LayoutPanel sourcePanel = new LayoutPanel();
+        sourcePanel.add(processEvents);        
+        tabPanel.add(processEvents, "Source");
+        
+        MessageBuilder.createCall(new RemoteCallback<List<String>>(){
+			
+        	public void callback(List<String> list) {
+        		final DefaultListModel<String> model = (DefaultListModel<String>)processEvents.getModel();
+        		model.clear();
+        		for (String value : list) {
+        			model.add(formatResult(value));
+        		}
+        	}
+        	
+        }, HistoryRecords.class).getAllEvents(inst.getId());
+        
+        tabPanel.selectTab(0);
+        
+        layout.add(tabPanel, new BoxLayoutData(BoxLayoutData.FillStyle.HORIZONTAL));
 
         diagramWindowPanel = new WidgetWindowPanel(
                 "Process Instance Activity",
                 layout, true
         );
+    }
+    
+    private String formatResult(String value) {
+    	StringBuffer sbuffer = new StringBuffer();
+    	StringTokenizer st = new StringTokenizer(value, "~");
+    	sbuffer.append(st.nextToken() + " : ");
+    	
+    	while (st.hasMoreTokens()) {
+    		sbuffer.append("<br/>");
+    		sbuffer.append(st.nextToken());
+    	}
+    	
+    	return sbuffer.toString();
     }
 
     private void createDataWindow(ProcessInstanceRef inst)
