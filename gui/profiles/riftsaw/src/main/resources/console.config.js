57f3ff6d41b28aa4db694b86b24685273cd3206e
Jeff Yu <jeff.yuchang@gmail.com>
2012-01-10 15:34:56 +1100

    * Updated the gwt-console-server war context-name configurable, this will avoid name confliction when deploying both riftsaw and jBPM into SwitchYard. * Updated the riftsaw's gwt-console from bpel-console-server path.

--- gui/profiles/riftsaw/src/main/resources/console.config.js
+++ gui/profiles/riftsaw/src/main/resources/console.config.js
@@ -1,7 +1,7 @@
 var consoleConfig = {
   profileName:          "BPEL Console", // don't change
   logo:                 "app/riftsaw_logo.png",
-  serverWebContext:     "/gwt-console-server",
+  serverWebContext:     "/bpel-console-server",
   defaultEditor:        "org.jboss.bpm.console.client.process.ProcessModule",
   overallReportFile:   "overall_activity.rptdesign",
   processSummaryReportFile: "process_summary.rptdesign",
